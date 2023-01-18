ba79eec5a4218e3969fc106c802bd6ce464c3b86
salwaawaaa <110708816+salwaawaaa@users.noreply.github.com>
2023-01-18 14:53:03 +0700

    Add files via upload

---
+++ pages/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>halaman depan admin</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ pages/admin/tambah.vue
@@ -0,0 +1,29 @@
+<template>
+  <navbar />
+  <div>
+    <h1>Bayar Kas</h1>
+    <form @submit.prevent="tambah()">
+      <input v-model="nama" placeholder="nama" /> <br />
+      <input v-model="tanggal" placeholder="tanggal" /> <br />
+      <input v-model="nominal" placeholder="nominal" /> <br />
+      <button type="submit">Tambah</button>
+      <NuxtLink to="/">Kembali</NuxtLink>
+    </form>
+  </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient();
+const nama = ref();
+const tanggal = ref();
+const nominal = ref();
+
+async function tambah() {
+  await supabase.from("kas").insert({
+    nama: nama.value,
+    tanggal: tanggal.value,
+    nominal: nominal.value,
+  });
+  navigateTo("/");
+}
+</script>
